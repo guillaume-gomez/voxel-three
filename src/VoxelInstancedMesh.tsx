0d9c78d9a678fc0b187360477abbe43b9186c378
Guillaume Gomez <guillaume.gomez846@gmail.com>
2024-06-10 18:09:53 +0200

    change material + size of the geometry

---
+++ src/VoxelInstancedMesh.tsx
@@ -0,0 +1,45 @@
+import { useRef , useEffect } from 'react';
+import { Object3D, InstancedMesh, MeshLambertMaterial,Vector3,DoubleSide } from 'three';
+import { RoundedBoxGeometry } from 'three/examples/jsm/geometries/RoundedBoxGeometry.js';
+
+
+interface VoxelInstancedMeshProps {
+  numberOfCubes: number;
+  voxelsData: Vector3[];
+}
+
+const SIZE = 0.2;
+const boxGeometry = new RoundedBoxGeometry(SIZE, SIZE, SIZE, 2, 0.03);
+
+
+const paramsMaterial = { roughness: 0.0, metalness: 0.2, emissive: 0x000000, castShadow: true, side: DoubleSide };
+const material =  new MeshLambertMaterial({color: "#BD2827", ...paramsMaterial})
+
+function VoxelInstancedMesh ({numberOfCubes, voxelsData} : VoxelInstancedMeshProps) {
+  const meshRef = useRef<InstancedMesh>(null);
+
+  useEffect(() => {
+    init();
+  }, [voxelsData])
+
+  function init() {
+      voxelsData.map((position, index) => {
+        const object = new Object3D();
+        object.position.set(position.x, position.y, position.z);
+        object.updateMatrix();
+        meshRef.current?.setMatrixAt(index, object.matrix);
+      })
+    meshRef.current.instanceMatrix.needsUpdate = true;
+  }
+
+  return (
+    <instancedMesh
+      receiveShadow={true}
+      castShadow={true}
+      ref={meshRef}
+      args={[boxGeometry, material, 1000 ]}
+    />
+  );
+};
+
+export default VoxelInstancedMesh;
