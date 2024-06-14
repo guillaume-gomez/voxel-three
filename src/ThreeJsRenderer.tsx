cfd1dc5ea1133a9ab1eb7e7f3d3a97e6ed2f71ee
Guillaume Gomez <guillaume.gomez846@gmail.com>
2024-06-14 18:01:53 +0200

    split the code to voxelise an object3d

--- src/ThreeJsRenderer.tsx
+++ src/ThreeJsRenderer.tsx
@@ -1,7 +1,7 @@
-import { Box3, Vector3, Object3D, Raycaster, DoubleSide } from "three";
+import {  Object3D, DoubleSide } from "three";
 import { useRef, useState } from 'react';
 import { Canvas } from '@react-three/fiber';
-import VoxelInstancedMesh from "./VoxelInstancedMesh";
+import Voxelizer from "./Voxelizer";
 import BoxHelperMesh from "./BoxHelperMesh";
 
 import { OrbitControls, Torus, Sphere,TorusKnot } from '@react-three/drei';
@@ -9,47 +9,14 @@ import { OrbitControls, Torus, Sphere,TorusKnot } from '@react-three/drei';
 
 function ThreeJsRenderer() {
     const [ gridSize ] = useState<number>(0.2);
-    const [geometriesType] = useState<string>("torus");
-    const [voxelsData, setVoxelsData] = useState<Vector3[]>([]);
-    const [randomize] = useState<boolean>(false);
-    const torusRef = useRef<Object3D>(null);
-
-    function voxelizeMesh(mesh: Object3D) {
-        const voxels = [];
-        const boundingBox = new Box3().setFromObject(mesh);
-        for (let i = boundingBox.min.x; i < boundingBox.max.x; i += gridSize) {
-            for (let j = boundingBox.min.y; j < boundingBox.max.y; j += gridSize) {
-                for (let k = boundingBox.min.z; k < boundingBox.max.z; k += gridSize) {
-                    const position = new Vector3(i, j, k);
-                    if (isInsideMesh(position, mesh)) {
-                        if(randomize) {
-                            voxels.push(new Vector3(
-                                position.x * Math.random(),
-                                position.y * Math.random(),
-                                position.z * Math.random(),
-                                )
-                            )
-                        } else {
-                            voxels.push(position)
-                        }
-                    }
-                }
-            }
-        }
-        setVoxelsData(voxels);
-    }
-
-    function isInsideMesh(position: Vector3 , mesh: Object3D) {
-        const rayCaster = new Raycaster();
-        rayCaster.set(position, new Vector3(0,-1,0));
-        const rayCasterIntersects = rayCaster.intersectObject(mesh, true);
-        // we need odd number of intersections
-        return rayCasterIntersects.length % 2 === 1;
-    }
+    const [geometriesType] = useState<string>("sphere");
+    const [randomizePosition] = useState<boolean>(false);
+    const objectRef = useRef<Object3D>(null);
+    const [selectedObject3D, setSelectedObject3D] = useState<Object3D| null>(null);
 
     return (
         <div style={{width:"100%", height: "75%"}}>
-            <button onClick={() => voxelizeMesh(torusRef!.current)}>Generate</button>
+            <button onClick={() => setSelectedObject3D(objectRef!.current)}>Generate</button>
             <Canvas
                 style={{background: "grey", width: 500, height: 500}}
                 //camera={{ position: [0,0, 1], fov: 75, far: 1000 }}
@@ -61,28 +28,34 @@ function ThreeJsRenderer() {
                     <ambientLight intensity={Math.PI / 2} />
                     <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} />
                     <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
-                    {geometriesType === "torus" &&
                     <BoxHelperMesh>
-                        <Torus
-                            args={[2, 1, 30, 30]}
-                            ref={torusRef}
-                        >
-                            <meshStandardMaterial color="blue" wireframe={true} side={DoubleSide} />
-                        </Torus>
+                        {geometriesType === "torus" &&
+                            <Torus
+                                args={[2, 1, 30, 30]}
+                                ref={objectRef}
+                            >
+                                <meshStandardMaterial color="blue" wireframe={true} side={DoubleSide} />
+                            </Torus>
+                        }
+                        {geometriesType === "torus knot" &&
+                            <TorusKnot
+                                args={[2, 0.6, 50, 10]}
+                                ref={objectRef}
+                            >
+                                <meshStandardMaterial color="purple" wireframe={true} side={DoubleSide} />
+                            </TorusKnot>
+                        }
+                        {geometriesType === "sphere" &&
+                            <Sphere ref={objectRef}>
+                              <meshStandardMaterial color="blue" wireframe={true} side={DoubleSide} />
+                            </Sphere>
+                        }
                     </BoxHelperMesh>
-                    }
-                    {geometriesType === "torus knot" &&
-                        <TorusKnot
-                            args={[2, 0.6, 50, 10]}
-                            ref={torusRef}
-                        />
-                    }
-                    {geometriesType === "sphere" &&
-                        <Sphere>
-                          <meshStandardMaterial color="green" wireframe={true} side={DoubleSide} />
-                        </Sphere>
-                    }
-                        <VoxelInstancedMesh voxelsData={voxelsData} />
+                    <Voxelizer
+                        object3D={selectedObject3D}
+                        gridSize={gridSize}
+                        randomizePosition={randomizePosition}
+                    />
                     <OrbitControls makeDefault />
             </Canvas>
       </div>
---
+++ src/Voxelizer.tsx
@@ -0,0 +1,62 @@
+import { Box3, Vector3, Object3D, Raycaster } from "three";
+import { useRef, useState, useEffect } from 'react';
+import VoxelInstancedMesh from "./VoxelInstancedMesh";
+
+interface VoxelizerProps {
+    object3D: Object3D;
+    gridSize?: number;
+    randomizePosition?: boolean
+}
+
+function Voxelizer({object3D, gridSize=0.2, randomizePosition=false}) {
+    const [voxelsData, setVoxelsData] = useState<Vector3[]>([]);
+    const torusRef = useRef<Object3D>(null);
+
+    useEffect(() => {
+        if(object3D) {
+            voxelizeMesh(object3D);
+        }
+    }, [object3D])
+
+    function voxelizeMesh(mesh: Object3D) {
+        const voxels = [];
+        const boundingBox = new Box3().setFromObject(mesh);
+        for (let i = boundingBox.min.x; i < boundingBox.max.x; i += gridSize) {
+            for (let j = boundingBox.min.y; j < boundingBox.max.y; j += gridSize) {
+                for (let k = boundingBox.min.z; k < boundingBox.max.z; k += gridSize) {
+                    const position = new Vector3(i, j, k);
+                    if (isInsideMesh(position, mesh)) {
+                        if(randomizePosition) {
+                            voxels.push(randomize(position));
+                        } else {
+                            voxels.push(position)
+                        }
+                    }
+                }
+            }
+        }
+        setVoxelsData(voxels);
+    }
+
+    function isInsideMesh(position: Vector3 , mesh: Object3D) {
+        const rayCaster = new Raycaster();
+        rayCaster.set(position, new Vector3(0,-1,0));
+        const rayCasterIntersects = rayCaster.intersectObject(mesh, true);
+        // we need odd number of intersections
+        return rayCasterIntersects.length % 2 === 1;
+    }
+
+    function randomize(position: Vector3) : Vector3 {
+        return new Vector3(
+            position.x + (Math.random() - 0.5),
+            position.y + (Math.random() - 0.5),
+            position.z + (Math.random() - 0.5),
+        );
+    }
+
+    return (
+        <VoxelInstancedMesh voxelsData={voxelsData} />
+    );
+}
+
+export default Voxelizer;
