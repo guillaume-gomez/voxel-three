7ba849c29cfaafb704124818a783d85bf5006211
Guillaume Gomez <guillaume.gomez846@gmail.com>
2024-06-16 13:20:11 +0200

    scale model size

--- src/Model.tsx
+++ src/Model.tsx
@@ -1,14 +1,25 @@
-import { Vector3 } from "three";
+import { useEffect } from "react";
+import { Vector3, Box3 } from "three";
 import { Sky, useGLTF } from '@react-three/drei';
 
 interface ModelProps {
-  path: string;
   position: Vector3;
   rotation: Vector3;
+  path?: string;
+  modelSize?: number;
 }
 
-function Model({ position, rotation, path = '/Donut.glb' }: ModelProps) {
+function Model({ position, rotation, path = '/Donut.glb', modelSize=8 }: ModelProps) {
   const { scene } = useGLTF(path);
+
+  useEffect(() => {
+    const boundingBox = new Box3().setFromObject(scene);
+    const size = boundingBox.getSize(new Vector3());
+    const scaleFactor = modelSize / size.length();
+    const center = boundingBox.getCenter(new Vector3()).multiplyScalar(-scaleFactor);
+
+    scene.scale.multiplyScalar(scaleFactor);
+  }, [scene]);
 
   return (
     <primitive
