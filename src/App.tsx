ccae8297bc5ae1787f19300cecc3ee66aee9a1a4
Guillaume Gomez <guillaume.gomez846@gmail.com>
2024-06-19 17:07:22 +0200

    remove unused code

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import { useState } from 'react';
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -6,9 +5,6 @@ import ThreeJsRenderer  from "./ThreeJsRenderer";
 
 
 function App() {
-  const [count, setCount] = useState(0)
-
-
   return (
     <>
       <div>
@@ -21,14 +17,6 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <ThreeJsRenderer />
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
