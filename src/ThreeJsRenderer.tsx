c460626a50aacf5baa070292c2e920668d1d980b
Guillaume Gomez <guillaume.gomez846@gmail.com>
2024-07-04 23:07:55 +0200

    use daisy-ui lib

--- src/ThreeJsRenderer.tsx
+++ src/ThreeJsRenderer.tsx
@@ -18,7 +18,7 @@ function ThreeJsRenderer() {
 
     return (
         <div style={{width:"100%", height: "75%"}}>
-            <button onClick={() => setSelectedObject3D(objectRef!.current)}>Generate</button>
+            <button className="btn btn-primary" onClick={() => setSelectedObject3D(objectRef!.current)}>Generate</button>
             <Canvas
                 style={{background: "grey", width: 500, height: 500}}
                 //shadowMapSoft={true}
